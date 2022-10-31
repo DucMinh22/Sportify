c5998f38cff1be714b98720e90312e0f2f47dc60
DucMinh22 <ducdudu22@gmail.com>
2022-10-31 10:33:42 +0700

    spilit code

---
+++ components/Banner/index.js
@@ -0,0 +1,139 @@
+import Image from 'next/image'
+const Banner = () => {
+    return (
+        <div className="home_main">
+            <div className="hm_gradient-1"></div>
+            <div className="hm_gradient-2"></div>
+            <div className="container hm_cont">
+                <span className="home_text">
+                    <small>Yakın Geçmişte Çalınan</small>
+                    <h1 className="">Geri atla</h1>
+                    <h3>Müziğini tam bıraktığın yerden devam ettir.</h3>
+                    <a className="green_btn" role="button" href="https://spotify-clone-oguz3.web.app/">Web Player'ı aç</a>
+                </span>
+                <span className="home_cards">
+                    <div className="hm_card opacity_anim">
+                        <div className="box">
+                            <Image src="/home_song-6.jpg" alt="" width={100} height={100}/>
+                        </div>
+                        <div className="box_details">
+                            <div className="play-icon">
+                                <div>
+                                    <svg id="album-play" viewBox="0 0 25.4 25.4">
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <h2>Türkçe Rock</h2>
+                        </div>
+                    </div>
+                    <div className="hm_card opacity_anim-1d">
+                        <div className="box">
+                            <Image src="/home_song-2.jpg" alt="" width={100} height={100}/>
+                        </div>
+                        <div className="box_details">
+                            <div className="play-icon">
+                                <div>
+                                    <svg id="album-play" viewBox="0 0 25.4 25.4">
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <h2>Your Top Songs 2020</h2>
+                        </div>
+                    </div>
+                    <div className="hm_card opacity_anim-2d">
+                        <div className="box">
+                            <Image src="/home_song-3.jpg" alt="" width={100} height={100}/>
+                        </div>
+                        <div className="box_details">
+                            <div className="play-icon">
+                                <div>
+                                    <svg id="album-play" viewBox="0 0 25.4 25.4">
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <h2>Purple Turtle</h2>
+                        </div>
+                    </div>
+                    <div className="hm_card opacity_anim">
+                        <div className="box">
+                            <Image src="/home_song-4.jpg" alt=""  width={100} height={100}/>
+                        </div>
+                        <div className="box_details">
+                            <div className="play-icon">
+                                <div>
+                                    <svg id="album-play" viewBox="0 0 25.4 25.4">
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <h2>Soft Rock</h2>
+                        </div>
+                    </div>
+                    <div className="hm_card opacity_anim-1d">
+                        <div className="box">
+                            <Image src="/home_song-5.jpg" alt="" width={100} height={100}/>
+                        </div>
+                        <div className="box_details">
+                            <div className="play-icon">
+                                <div>
+                                    <svg id="album-play" viewBox="0 0 25.4 25.4">
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <h2>NKVT</h2>
+                        </div>
+                    </div>
+                    <div className="hm_card opacity_anim-2d">
+                        <div className="box">
+                            <Image src="/home_song-1.jpg" alt="" width={100} height={100}/>
+                        </div>
+                        <div className="box_details">
+                            <div className="play-icon">
+                                <div>
+                                    <svg id="album-play" viewBox="0 0 25.4 25.4">
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                        <path fill="#ffffff" d="M12.7,25.4A12.7,12.7,0,1,1,25.4,12.7,12.71,12.71,0,0,1,12.7,25.4Zm0-24A11.3,11.3,0,1,0,24,12.7,11.31,11.31,0,0,0,12.7,1.4Z"></path>
+                                        <path fill="#ffffff" d="M11,16.47,17,13a.34.34,0,0,0,0-.59L11,8.93a.34.34,0,0,0-.51.29v7A.34.34,0,0,0,11,16.47Z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <h2>Objektif</h2>
+                        </div>
+                    </div>
+                </span>
+            </div>
+            <div className="container">
+                <div className="arrow-scroll-down slide-top">
+                    <div>
+                        <svg id="arrow-scroll-down" viewBox="0 0 32 32">
+                            <polygon fill="#ffffff" points="15.98 22.43 7.28 13.73 8.7 12.32 15.98 19.6 23.28 12.3 24.7 13.72 15.98 22.43"></polygon>
+                            <path fill="#ffffff" d="M16,32A16,16,0,1,1,32,16,16,16,0,0,1,16,32ZM16,2A14,14,0,1,0,30,16,14,14,0,0,0,16,2Z"></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
